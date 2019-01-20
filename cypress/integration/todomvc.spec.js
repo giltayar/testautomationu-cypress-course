e29878a622d855114f3ecbf376b2a8b56b77963d
Gil Tayar <gil@tayar.org>
2019-01-20 13:44:26 +0200

    taught cy.get/contains/click/type

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,11 @@
 /// <reference types="cypress" />
 
-it('should navigate to the TodoMVC App', () => {
+it('should add a new todo to the list', () => {
   cy.visit('http://todomvc-app-for-testing.surge.sh/')
+
+  cy.get('.new-todo', {timeout: 6000}).type('Clean room{enter}')
+
+  cy.get('.toggle').click()
+
+  cy.contains('Clear completed').click()
 })
