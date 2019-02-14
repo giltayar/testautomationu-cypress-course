f96270d7a94eab697ce564d798c2078701607b75
Gil Tayar <gil@tayar.org>
2019-02-14 11:41:48 +0200

    refactored page object to be page module

--- cypress/integration/todo-filtering.spec.js
+++ cypress/integration/todo-filtering.spec.js
@@ -1,34 +1,40 @@
 /// <reference types="cypress" />
-import {TodoPage} from '../page-objects/todo-page'
+import {
+  navigate,
+  addTodo,
+  toggleTodo,
+  showOnlyActiveTodos,
+  showOnlyCompletedTodos,
+  showAllTodos,
+  validateNumberOfTodosShown,
+} from '../page-objects/todo-page'
 
 describe('filtering', function() {
-  const todoPage = new TodoPage()
-
   beforeEach(() => {
-    todoPage.navigate()
+    navigate()
 
-    todoPage.addTodo('Clean room')
-    todoPage.addTodo('Learn JavaScript')
-    todoPage.addTodo('Use Cypress')
+    addTodo('Clean room')
+    addTodo('Learn JavaScript')
+    addTodo('Use Cypress')
 
-    todoPage.toggleTodo(1)
+    toggleTodo(1)
   })
 
   it('should filter "Active" correctly', () => {
-    todoPage.showOnlyActiveTodos()
+    showOnlyActiveTodos()
 
-    todoPage.validateNumberOfTodosShown(2)
+    validateNumberOfTodosShown(2)
   })
 
   it('should filter "Completed" correctly', () => {
-    todoPage.showOnlyCompletedTodos()
+    showOnlyCompletedTodos()
 
-    todoPage.validateNumberOfTodosShown(1)
+    validateNumberOfTodosShown(1)
   })
 
   it('should filter "All" correctly', () => {
-    todoPage.showAllTodos()
+    showAllTodos()
 
-    todoPage.validateNumberOfTodosShown(3)
+    validateNumberOfTodosShown(3)
   })
 })
