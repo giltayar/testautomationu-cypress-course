cd5c93ede91073f9bfbe5719719edece5986effd
Gil Tayar <gil@tayar.org>
2019-08-13 12:47:42 +0300

    visual testing

--- cypress/integration/todo-visuals.spec.js
+++ cypress/integration/todo-visuals.spec.js
@@ -2,14 +2,20 @@
 import * as todoPage from '../page-objects/todo-page'
 
 describe('visual validation', () => {
-  before(() =>  todoPage.navigate())
+  before(() =>  cy.visit('http://todomvc-app-for-testing.surge.sh/?different-title-color'))
 
-  beforeEach(() => {
+  beforeEach(() =>
     cy.eyesOpen({
       appName: 'TAU TodoMVC',
       batchName: 'TAU TodoMVC',
+      browser: [
+        {name: 'chrome', width: 1024, height: 768},
+        {name: 'chrome', width: 800, height: 600},
+        {name: 'firefox', width: 1024, height: 768},
+        {deviceName: 'iPhone X'},
+      ]
     })
-  })
+  )
 
   afterEach(() => cy.eyesClose())
 
