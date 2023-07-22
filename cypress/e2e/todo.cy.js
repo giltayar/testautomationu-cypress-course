82b14892b7c79795691ee0d31a4ec88bf995d30e
khmdagal <khmdagal@gmail.com>
2023-07-22 23:19:27 +0100

    write test - find element - type value - checked the value - checked the check button

---
+++ cypress/e2e/todo.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('template spec', () => {
+  it('should be able to add new element to the list', () => {
+    cy.visit('https://todomvc-app-for-testing.surge.sh')
+  })
+})
--- cypress/integration/todomvs.spec.js
+++ cypress/integration/todomvs.spec.js
@@ -1,5 +1,11 @@
 /// <reference types="cypress" />
 
 it("should be able to add new element to the list", () => {
-    cy.visit('https://todomvc-app-for-testing.surge.sh')
+    cy.visit('https://todomvc-app-for-testing.surge.sh/')
+    cy.get('.new-todo', {timeout: 6000}).type('Learn Cypress {enter}')
+    cy.get("label").should("have.text", "Learn Cypress")
+    cy.get('.toggle').should('not.be.checked')
+    cy.get('.toggle').click()
+    cy.get("label").should("have.css", "")
+    cy.get("label").should("have.css", "text-decoration", "line-through")
 })
