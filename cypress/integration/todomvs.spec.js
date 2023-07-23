2af8510b8a965eb5f3e23b3364f9ab5e924fff5b
khmdagal <khmdagal@gmail.com>
2023-07-23 14:51:27 +0100

    Group and add beforeEach function

--- cypress/integration/todomvs.spec.js
+++ cypress/integration/todomvs.spec.js
@@ -1,11 +1,24 @@
 /// <reference types="cypress" />
 
-it("should be able to add new element to the list", () => {
-    cy.visit('https://todomvc-app-for-testing.surge.sh/')
-    cy.get('.new-todo', {timeout: 6000}).type('Learn Cypress {enter}')
-    cy.get("label").should("have.text", "Learn Cypress")
+describe("todo actions", () => {
+
+    beforeEach(() => { 
+            cy.visit('https://todomvc-app-for-testing.surge.sh/')
+            cy.get('.new-todo', {timeout: 6000}).type('Learn Cypress {enter}')
+    })
+  it('should be able to add new element to the list', () => {
+    cy.get('label').should('have.text', 'Learn Cypress')
     cy.get('.toggle').should('not.be.checked')
+  })
+
+    it('should complete the task with line through the content', () => {
     cy.get('.toggle').click()
-    cy.get("label").should("have.css", "")
-    cy.get("label").should("have.css", "text-decoration", "line-through")
+    cy.get('label').should('have.css', 'text-decoration', 'line-through solid rgb(217, 217, 217)')
+  })
+
+    it('should clear the list', () => {
+       cy.get('.toggle').click()
+    cy.get('.clear-completed').click()
+    cy.get('.todo-list').should('not.have.descendants', 'li')
+  })
 })
