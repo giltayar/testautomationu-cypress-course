88d3a48e4c8499e16a9b65f122f0969867ca4733
khmdagal <khmdagal@gmail.com>
2023-07-23 20:14:37 +0100

    Add open and close and 3 screenshot check

--- cypress/integration/todo-visual.spec.js
+++ cypress/integration/todo-visual.spec.js
@@ -4,10 +4,28 @@ import * as todoPage from '../page-objects/todo-page'
 describe('visual validation', () => {
   before(() => todoPage.navigate())
 
+  beforeEach(() =>
+    cy.eyesOpen({
+      appName: 'TUA  TodoMVC',
+      batchName: 'TAU TodoMVC Hey! Khadar did it',
+      
+    }),
+  )
+  afterEach(() => cy.eyesClose())
+
   it('should look good', () => {
+    // Adding first screen shot check
+    cy.eyesCheckWindow('empty todo list')
+
     todoPage.addTodo('Cypress')
     todoPage.addTodo('JavaScript')
 
+    // second screen shot check
+    cy.eyesCheckWindow('Two todos')
+
     todoPage.toggleTodo(0)
+
+    // third screen shot check
+    cy.eyesCheckWindow('Mark as Completed')
   })
 })
